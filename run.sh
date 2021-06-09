6ba6f1cac21aed55c178db1f24b8402b974388b7
ProjectTL12345 <projecttl12345@gmail.com>
2021-06-09 17:02:08 +0900

    first commit

---
+++ run.sh
@@ -0,0 +1,8 @@
+while :
+do
+	python3 bot.py
+	echo "Press [CTRL+C] to stop..."
+	sh update.sh update
+	
+	sleep 1
+done
---
+++ update.sh
@@ -0,0 +1,9 @@
+# shellcheck disable=SC2005
+if [ "$1" == "update" ]; then
+echo "$(git pull origin master)"
+elif [ "$1" == "push" ]; then
+  echo "$(git commit -m "script: push by remote server")"
+  echo "$(git push -u origin master)"
+else
+  echo "$1 not available"
+fi
