0ba4363ef33bad4d67283aab5c366faa73ea3dcb
Project_TL <projecttl@Project-TLs-MacBook-Pro.local>
2021-06-09 23:56:59 +0900

    feat: add help command

---
+++ help.py
@@ -0,0 +1,23 @@
+import discord, asyncio
+from discord import embeds
+from discord.ext import commands
+
+def setup(bot):
+    bot.add_cog(Help(bot))
+
+class Help(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.embed_color = 0x75B8FF
+
+    @commands.command(name="help")
+    async def help(self, ctx):
+        embed = discord.Embed(title=":dart: **Help**", description="", color=self.embed_color)
+        embed.add_field(name="`\\help`", value="Open help embed", inline=True)
+        embed.add_field(name="`\\ping`", value="ping with your bot", inline=True)
+        embed.add_field(name="`\\pong`", value="pong with your bot", inline=True)
+        embed.add_field(name="`\\carrot`", value="Help! :carrot:", inline=True)
+        embed.add_field(name="`\\reload`", value="Reload command", inline=True)
+        embed.add_field(name="`\\reboot`", value="Reboot command **Update bot only**", inline=True)
+
+        await ctx.send(embed=embed)
