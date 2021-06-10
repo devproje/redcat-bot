cf6972d438dc6d2c2001c14c4f86fee4ce0d0ce9
ProjectTL12345 <projecttl12345@gmail.com>
2021-06-10 15:54:23 +0900

    fix: change name

--- Cogs/wint_choco.py
+++ Cogs/wint_choco.py
@@ -2,15 +2,15 @@ import discord, os, asyncio
 from discord.ext import commands
 
 def setup(bot):
-    bot.add_cog(WintChoco(bot))
+    bot.add_cog(WinCho(bot))
 
-class WintChoco(commands.Cog):
+class WinCho(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.embed_color = 0x75B8FF
 
-    @commands.command(name="wintchoco")
-    async def wint_choco(self, ctx, wincho_action):
+    @commands.command(name="wincho")
+    async def wincho(self, ctx, wincho_action):
         if wincho_action == "smash":
             embed = discord.Embed(description="**윈초 부수기!**", color=self.embed_color)
             embed.set_image(url="https://d2culxnxbccemt.cloudfront.net/craft/content/uploads/2020/07/30221830/image-001.jpg")
@@ -22,7 +22,7 @@ class WintChoco(commands.Cog):
             embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
 
-    @wint_choco.error
+    @wincho.error
     async def choco_error(self, ctx, error):
         embed = discord.Embed(title="Error!", descroption=f"{error} is not found!", color=self.embed_color)
         embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
