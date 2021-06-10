e64720a4aef66bd117c55b09da5a0fd65af944a7
ProjectTL12345 <projecttl12345@gmail.com>
2021-06-10 12:56:36 +0900

    debug: visual get user data

--- bot.py
+++ bot.py
@@ -30,20 +30,20 @@ async def on_ready():
 async def reload_commands(ctx, extension=None):
     print(f"name: {ctx.author.name}, id: {ctx.author.id}")
 
-    if check_user(ctx.author.name, ctx.author.id):
-        if extension is None:
-            for filename in os.listdir("Cogs"):
-                if filename.endswith(".py"):
-                    bot.unload_extension(f"Cogs.{filename[:-3]}")
-                    bot.load_extension(f"Cogs.{filename[:-3]}")
+    # if check_user(ctx.author.name, ctx.author.id):
+    if extension is None:
+        for filename in os.listdir("Cogs"):
+            if filename.endswith(".py"):
+                bot.unload_extension(f"Cogs.{filename[:-3]}")
+                bot.load_extension(f"Cogs.{filename[:-3]}")
 
-            await ctx.send(":white_check_mark: reload complete!")
+        await ctx.send(":white_check_mark: reload complete!")
 
-        else:
-            bot.unload_extension(f"Cogs.{extension}")
-            bot.load_extension(f"Cogs.{extension}")
-            await ctx.send(f":white_check_mark: {extension} has reloaded!")
     else:
-        await ctx.send(":stop_sign: Error!")
+        bot.unload_extension(f"Cogs.{extension}")
+        bot.load_extension(f"Cogs.{extension}")
+        await ctx.send(f":white_check_mark: {extension} has reloaded!")
+    # else:
+        # await ctx.send(":stop_sign: Error!")
 
 bot.run(token)
