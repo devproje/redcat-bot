a7b067ec1ef9d18ce08951a840f3ed13e2343379
ProjectTL12345 <projecttl12345@gmail.com>
2021-06-10 15:37:52 +0900

    fix: choco

--- Cogs/wint_choco.py
+++ Cogs/wint_choco.py
@@ -10,7 +10,7 @@ class WintChoco(commands.Cog):
         self.embed_color = 0x75B8FF
 
     @commands.command(name="wintchoco")
-    async def wint_choco(self, ctx, wincho_action):
+    async def wintchoco(self, ctx, wincho_action):
         if wincho_action == "smash":
             embed = discord.Embed(description="**윈초 부수기!**", color=self.embed_color)
             embed.set_image(url="https://d2culxnxbccemt.cloudfront.net/craft/content/uploads/2020/07/30221830/image-001.jpg")
@@ -18,11 +18,5 @@ class WintChoco(commands.Cog):
         elif wincho_action == "melt":
             embed = discord.Embed(description="**윈초 녹이기!**", color=self.embed_color)
             embed.set_image(url="https://ak.picdn.net/shutterstock/videos/1014557474/thumb/1.jpg")
-            await ctx.send(embed=embed)
-        elif wincho_action == None:
-            embed = discord.Embed(title=":stop_sign: Error!", descroption=f"You must type argument!", color=self.embed_color)
-            await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(title=":stop_sign: Error!", descroption=f"{wincho_action} is not found!", color=self.embed_color)
             await ctx.send(embed=embed)
         
