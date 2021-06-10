6ad74d6c2e690325fcd7738aa1b0e4356eaa6442
ProjectTL12345 <projecttl12345@gmail.com>
2021-06-10 15:20:59 +0900

    fix: choco

--- Cogs/wint_choco.py
+++ Cogs/wint_choco.py
@@ -10,14 +10,19 @@ class WintChoco(commands.Cog):
         self.embed_color = 0x75B8FF
 
     @commands.command("wintchoco")
-    async def wint_choco(self, ctx, action):
-        if action == "smash":
+    async def wint_choco(self, ctx, wincho_action):
+        if wincho_action == "smash":
             embed = discord.Embed(description="**윈초 부수기!**", color=self.embed_color)
             embed.set_image(url="https://d2culxnxbccemt.cloudfront.net/craft/content/uploads/2020/07/30221830/image-001.jpg")
             await ctx.send(embed=embed)
-        elif action == "melt":
+        elif wincho_action == "melt":
             embed = discord.Embed(description="**윈초 녹이기!**", color=self.embed_color)
             embed.set_image(url="https://ak.picdn.net/shutterstock/videos/1014557474/thumb/1.jpg")
             await ctx.send(embed=embed)
         else:
             return None
+
+    @wint_choco.error()
+    async def choco_error(self, ctx, error):
+        embed = discord.Embed(title="Error!", descroption=f"{error} is not found!", color=self.embed_color)
+        await ctx.send(embed=embed)
