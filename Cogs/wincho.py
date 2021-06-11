2f5001f7a93ddb68a86282cd95d452f697958d5f
ProjectTL12345 <projecttl12345@gmail.com>
2021-06-11 18:16:32 +0900

    feat: Random mention

--- Cogs/wincho.py
+++ Cogs/wincho.py
@@ -1,62 +1,89 @@
-import discord, os, asyncio
+import discord, random, os, asyncio
 from discord.ext import commands
 
 def setup(bot):
     bot.add_cog(WinCho(bot))
+
+
+def wincho_mention(mention_type):
+    mention = "<@!602733713842896908>"
+    if mention_type == 0:
+        return f"{mention} 빨리 와바"
+    elif mention_type == 1:
+        return f"{mention} 빨리 안와?"
+    elif mention_type == 2:
+        return f"{mention} 야 윈초야 큰일났어 빨리 와봐!!"
+    elif mention_type == 3:
+        return f"{mention} 윈초 뭐해?"
+    elif mention_type == 4:
+        return f"{mention} 윈초 빼애애애애액"
+    elif mention_type == 5:
+        return f"{mention} 윈트초코 뭐해?"
+
 
 class WinCho(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.embed_color = 0xFFF1D6
 
+        self.melting = "https://ak.picdn.net/shutterstock/videos/1014557474/thumb/1.jpg"
+        self.smashing = "https://d2culxnxbccemt.cloudfront.net/craft/content/uploads/2020/07/30221830/image-001.jpg"
+        self.burning = "http://netherald.kro.kr:8080/apps/files_sharing/publicpreview/DSmMZADoMxB2kGM?x=1457&y=383&a=true&file=Burnt_Wintchoco.jpg&scalingup=0"
+
     @commands.command(name="wincho")
     async def wincho(self, ctx, wincho_action):
         if wincho_action == "smash":
             embed = discord.Embed(description="**Smashing Wincho!**", color=self.embed_color)
-            embed.set_image(url="https://d2culxnxbccemt.cloudfront.net/craft/content/uploads/2020/07/30221830/image-001.jpg")
-            embed.set_footer(text=f"{ctx.author.name}{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            embed.set_image(url=self.smashing)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif wincho_action == "melt":
             embed = discord.Embed(description="**Melting Wincho!**", color=self.embed_color)
-            embed.set_image(url="https://ak.picdn.net/shutterstock/videos/1014557474/thumb/1.jpg")
-            embed.set_footer(text=f"{ctx.author.name}{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            embed.set_image(url=self.melting)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif wincho_action == "call":
-            await ctx.channel.send("<@!602733713842896908> 빨리와바")
+            await ctx.channel.send(wincho_mention(random.randrange(0, 5)))
         elif wincho_action == "burn":
             embed = discord.Embed(description="**Burning Wincho!**", color=self.embed_color)
-            embed.set_image(url="http://netherald.kro.kr:8080/apps/files_sharing/publicpreview/DSmMZADoMxB2kGM?x=1457&y=383&a=true&file=Burnt_Wintchoco.jpg&scalingup=0")
+            embed.set_image(url=self.burning)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        elif wincho_action is None:
+            embed = discord.Embed(title=":question: Wincho help",
+                                  descroption=f"`\\wincho <burn | smash | melt | call>`", color=self.embed_color)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(title="Error!", descroption=f"{wincho_action} is not found!", color=self.embed_color)
             embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
 
     @commands.command(name="윈초")
-    async def wincho_korean(self, ctx, wincho_action):
+    async def wincho_korean(self, ctx, wincho_action=None):
         if wincho_action == "부수기":
             embed = discord.Embed(description="**윈초 부수기!**", color=self.embed_color)
-            embed.set_image(url="https://d2culxnxbccemt.cloudfront.net/craft/content/uploads/2020/07/30221830/image-001.jpg")
+            embed.set_image(url=self.smashing)
             embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif wincho_action == "녹이기":
             embed = discord.Embed(description="**윈초 녹이기!**", color=self.embed_color)
-            embed.set_image(url="https://ak.picdn.net/shutterstock/videos/1014557474/thumb/1.jpg")
+            embed.set_image(url=self.melting)
             embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif wincho_action == "부르기":
-            await ctx.channel.send("<@!602733713842896908> 빨리와바")
+            await ctx.channel.send(wincho_mention(random.randrange(0, 5)))
         elif wincho_action == "태우기":
             embed = discord.Embed(description="**윈초 태우기!**", color=self.embed_color)
-            embed.set_image(url="http://netherald.kro.kr:8080/apps/files_sharing/publicpreview/DSmMZADoMxB2kGM?x=1457&y=383&a=true&file=Burnt_Wintchoco.jpg&scalingup=0")
+            embed.set_image(url=self.burning)
             embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
-
-    @wincho.error
-    async def choco_error(self, ctx, error):
-        embed = discord.Embed(title="Error!", descroption=f"{error} is not found!", color=self.embed_color)
-        embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
-        await ctx.send(embed=embed)
-
-    @wincho_korean.error
-    async def choco_error_korea(self, ctx, error):
-        embed = discord.Embed(title="오류발생!", descroption=f"{error}가 존재하지 않습니다!", color=self.embed_color)
-        embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
-        await ctx.send(embed=embed)
+        elif wincho_action is None:
+            embed = discord.Embed(title=":question: 윈초 커맨드 도움말", descroption=f"`\\윈초 <녹이기 | 부수기 | 녹이기 | 부르기>`",
+                                  color=self.embed_color)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(title="오류발생!", descroption=f"{wincho_action}은(는) 존재하지 않습니다!", color=self.embed_color)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
