7d4644160e0bc067487fcd8f7dd5d52095040605
ProjectTL12345 <projecttl12345@gmail.com>
2021-06-13 01:40:02 +0900

    fix: amount bug

--- Cogs/chat_cleaner.py
+++ Cogs/chat_cleaner.py
@@ -12,12 +12,12 @@ class ChatCleaner(commands.Cog):
     @commands.command(name="clear")
     async def chat_cleaner(self, ctx, amount: int = None):
         if ctx.author.id == 415801068174180352 or ctx.author.guild_permissions.administrator:
-            if amount > 1 or amount == None:
+            if amount > 1:
                 embed = discord.Embed(title=":white_check_mark: **Chat Removed**", description=f"{amount} chats is removed!", color=self.embed_color)
                 await ctx.channel.purge(limit=amount + 2)
                 embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
                 await ctx.send(embed=embed)
-            elif amount == 1:
+            elif amount == 1 or amount == None :
                 embed = discord.Embed(title=":white_check_mark: **Chat Removed**", description=f"{amount} chat is removed!", color=self.embed_color)
                 await ctx.channel.purge(limit=3)
                 embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
