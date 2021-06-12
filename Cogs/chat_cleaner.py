692a59c8d4bc11f7c29d2ff17d758b96e5883abb
ProjectTL12345 <projecttl12345@gmail.com>
2021-06-13 01:24:44 +0900

    fix: number count

--- Cogs/chat_cleaner.py
+++ Cogs/chat_cleaner.py
@@ -12,9 +12,14 @@ class ChatCleaner(commands.Cog):
     @commands.command(name="clear")
     async def chat_cleaner(self, ctx, amount: int = None):
         if ctx.author.id == 415801068174180352:
-            if amount > 0:
+            if amount > 1:
                 await ctx.channel.purge(limit=amount)
-                embed = discord.Embed(title=":white_check_mark: **Chat Removed**", description=f"{amount} chat(s) is removed!", color=self.embed_color)
+                embed = discord.Embed(title=":white_check_mark: **Chat Removed**", description=f"{amount} chats is removed!", color=self.embed_color)
+                embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+                await ctx.send(embed=embed)
+            elif amount == 1:
+                await ctx.channel.purge(limit=amount)
+                embed = discord.Embed(title=":white_check_mark: **Chat Removed**", description=f"{amount} chat is removed!", color=self.embed_color)
                 embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
                 await ctx.send(embed=embed)
             elif amount > 300:
