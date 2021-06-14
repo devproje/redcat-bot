9088a1ea96ea0bce4a3ad645bf1c6298c56bfb77
Project_TL <projecttl12345@gmail.com>
2021-06-14 04:31:17 +0000

    feat: music function

--- Cogs/music.py
+++ Cogs/music.py
@@ -11,8 +11,10 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 class VoiceError(Exception):
     pass
 
+
 class YTDLError(Exception):
     pass
+
 
 class YTDLSource(discord.PCMVolumeTransformer):
     YTDL_OPTIONS = {
@@ -133,13 +135,13 @@ class Song:
     def create_embed(self):
         embed = (discord.Embed(title='Now playing',
                                description='```css\n{0.source.title}\n```'.format(self),
-                               color=0x75B8FF)
+                               color=discord.Color.blurple())
                  .add_field(name='Duration', value=self.source.duration)
                  .add_field(name='Requested by', value=self.requester.mention)
                  .add_field(name='Uploader', value='[{0.source.uploader}]({0.source.uploader_url})'.format(self))
                  .add_field(name='URL', value='[Click]({0.source.url})'.format(self))
-                 .set_thumbnail(url=self.source.thumbnail)
-                 .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+                 .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+                 .set_thumbnail(url=self.source.thumbnail))
 
         return embed
 
@@ -249,7 +251,7 @@ class Music(commands.Cog):
         self.bot = bot
         self.voice_states = {}
 
-        self.embed_color=0x75B8FF
+        self.embed_color = 0x75B8FF
 
     def get_voice_state(self, ctx: commands.Context):
         state = self.voice_states.get(ctx.guild.id)
@@ -275,18 +277,9 @@ class Music(commands.Cog):
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
         await ctx.send('An error occurred: {}'.format(str(error)))
 
-    @commands.command(name='join', invoke_without_subcommand=True)
-    async def _join(self, ctx: commands.Context):
-        destination = ctx.author.voice.channel
-        if ctx.voice_state.voice:
-            await ctx.voice_state.voice.move_to(destination)
-            return
-
-        ctx.voice_state.voice = await destination.connect()
-
     @commands.command(name="music")
     async def help(self, ctx, argument=None):
-        if argument is None:
+        if argument == "help":
             embed = (discord.Embed(title="**Music Help**", description="This is music options!", color=self.embed_color)
                 .add_field(name="`\\play <url or name>`", value="Play music", inline=True)
                 .add_field(name="`\\pause`", value="Pause music", inline=True)
@@ -300,10 +293,35 @@ class Music(commands.Cog):
                 .add_field(name="`\\loop`", value="Loop current music", inline=True)
                 .add_field(name="`\\remove <array_number>`", value="Remove queued target music", inline=True)
                 .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
-            return embed
+            await ctx.send(embed=embed)
+
+        elif argument == None:
+            return None
 
         else:
-            embed = discord.Embed()
+            return None
+
+    @commands.command(name='join', invoke_without_subcommand=True)
+    async def _join(self, ctx: commands.Context):
+        destination = ctx.author.voice.channel
+        if ctx.voice_state.voice:
+            await ctx.voice_state.voice.move_to(destination)
+            return
+
+        ctx.voice_state.voice = await destination.connect()
+
+    @commands.command(name='summon')
+    @commands.has_permissions(manage_guild=True)
+    async def _summon(self, ctx: commands.Context, *, channel: discord.VoiceChannel = None):
+        if not channel and not ctx.author.voice:
+            raise VoiceError('You are neither connected to a voice channel nor specified a channel to join.')
+
+        destination = channel or ctx.author.voice.channel
+        if ctx.voice_state.voice:
+            await ctx.voice_state.voice.move_to(destination)
+            return
+
+        ctx.voice_state.voice = await destination.connect()
 
     @commands.command(name='leave', aliases=['disconnect'])
     @commands.has_permissions(manage_guild=True)
@@ -325,15 +343,19 @@ class Music(commands.Cog):
             return await ctx.send('Volume must be between 0 and 100')
 
         ctx.voice_state.volume = volume / 100
-        await ctx.send(f'Volume of the player set to {volume}%')
+        await ctx.send('Volume of the player set to {}%'.format(volume))
 
     @commands.command(name='now', aliases=['current', 'playing'])
     async def _now(self, ctx: commands.Context):
+        """Displays the currently playing song."""
+
         await ctx.send(embed=ctx.voice_state.current.create_embed())
 
     @commands.command(name='pause')
     @commands.has_permissions(manage_guild=True)
     async def _pause(self, ctx: commands.Context):
+        """Pauses the currently playing song."""
+
         if not ctx.voice_state.is_playing and ctx.voice_state.voice.is_playing():
             ctx.voice_state.voice.pause()
             await ctx.message.add_reaction('⏯')
@@ -341,6 +363,8 @@ class Music(commands.Cog):
     @commands.command(name='resume')
     @commands.has_permissions(manage_guild=True)
     async def _resume(self, ctx: commands.Context):
+        """Resumes a currently paused song."""
+
         if not ctx.voice_state.is_playing and ctx.voice_state.voice.is_paused():
             ctx.voice_state.voice.resume()
             await ctx.message.add_reaction('⏯')
@@ -362,10 +386,12 @@ class Music(commands.Cog):
         await ctx.message.add_reaction('⏭')
         ctx.voice_state.skip()
 
-    @commands.command(name='queue')
+    @commands.command(name='queue', aliases=["q"])
     async def _queue(self, ctx: commands.Context, *, page: int = 1):
         if len(ctx.voice_state.songs) == 0:
-            return await ctx.send('Empty queue.')
+            embed=(discord.Embed(title=":no_entry: **Error!**", description="Empty queue.", color=self.embed_color)
+                .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+            return await ctx.send(embed=embed)
 
         items_per_page = 10
         pages = math.ceil(len(ctx.voice_state.songs) / items_per_page)
@@ -375,24 +401,30 @@ class Music(commands.Cog):
 
         queue = ''
         for i, song in enumerate(ctx.voice_state.songs[start:end], start=start):
-            queue += f'`{i + 1}.` [**{song.source.title}**]({song.source.url})\n'
+            queue += '`{0}.` [**{1.source.title}**]({1.source.url})\n'.format(i + 1, song)
 
-        embed = (discord.Embed(description=f'**{len(ctx.voice_state.songs)} tracks:**\n\n{queue}')
-                 .set_footer(text=f'Viewing page {page}/{pages}'))
+        embed = (discord.Embed(title=":notepad_spiral: **Queue List**", description=f"**Showing {page} page**", color=self.embed_color)
+                .add_field(name='{} tracks:\n\n{}'.format(len(ctx.voice_state.songs), queue), value='page {}/{}'.format(page, pages), inline=True)
+                .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+
         await ctx.send(embed=embed)
 
     @commands.command(name='shuffle')
     async def _shuffle(self, ctx: commands.Context):
         if len(ctx.voice_state.songs) == 0:
-            return await ctx.send('Empty queue.')
+            embed=(discord.Embed(title=":no_entry: **Error!**", description="Empty queue.", color=self.embed_color)
+                .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+            return await ctx.send(embed=embed)
 
         ctx.voice_state.songs.shuffle()
         await ctx.message.add_reaction('✅')
 
-    @commands.command(name='remove')
+    @commands.command(name='remove', aliases=["rm"])
     async def _remove(self, ctx: commands.Context, index: int):
         if len(ctx.voice_state.songs) == 0:
-            return await ctx.send('Empty queue.')
+            embed=(discord.Embed(title=":no_entry: **Error!**", description="Empty queue.", color=self.embed_color)
+                .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+            return await ctx.send(embed=embed)
 
         ctx.voice_state.songs.remove(index - 1)
         await ctx.message.add_reaction('✅')
@@ -405,7 +437,7 @@ class Music(commands.Cog):
         ctx.voice_state.loop = not ctx.voice_state.loop
         await ctx.message.add_reaction('✅')
 
-    @commands.command(name='play', aliases=['p'])
+    @commands.command(name='play', aliases=["p"])
     async def _play(self, ctx, *, search: str):
         if not ctx.voice_state.voice:
             await ctx.invoke(self._join)
@@ -414,12 +446,16 @@ class Music(commands.Cog):
             try:
                 source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop)
             except YTDLError as e:
-                await ctx.send(f'An error occurred while processing this request: {str(e)}')
+                embed=(discord.Embed(title=":no_entry: Error!", description=f"An error occurred while processing this request: {str(e)}")
+                    .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+                await ctx.send(embed=embed)
             else:
                 song = Song(source)
+                successful_embed=(discord.Embed(title=":white_check_mark: **Queue Successful!**", description=f"Enqueued {str(source)}", color=self.embed_color)
+                    .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
 
                 await ctx.voice_state.songs.put(song)
-                await ctx.send(f'Enqueued {str(source)}')
+                await ctx.send(embed=successful_embed)
 
     @_join.before_invoke
     @_play.before_invoke
