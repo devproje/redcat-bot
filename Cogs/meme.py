9cd3ba85531f6880750822b55316447df39073c1
ProjectTL12345 <projecttl12345@gmail.com>
2021-06-15 22:22:11 +0900

    feat: meme function

--- Cogs/meme.py
+++ Cogs/meme.py
@@ -6,16 +6,35 @@ def setup(bot):
 
 def image_selector(number):
     if number == None:
-        return
-
-    # TODO ADD SOME MEME IMAGE
+        return None
+    elif number == 1:
+        return "http://projecttl.net:8080/s/YZT8jk8GTgk7gb5/download/%EC%A0%95%EC%9D%98%EC%9D%98%20%ED%9B%84%ED%8F%AD%ED%92%8D.gif"
+    elif number == 2:
+        return "http://projecttl.net:8080/s/T3e9ZCzBBzGgy4H/download/Nuclear.gif"
+    elif number == 3:
+        return "http://projecttl.net:8080/s/aJCjtazJYWJpBo8/download/boom.gif"
 
 class Mime(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="짤")
+    @commands.command(name="meme")
     async def _meme(self, ctx, argument=None):
         if argument is None:
-            # TODO GENERATED MEME COMMAND
-            await ctx.send("None")
+            embed = discord.Embed(title=":question: 밈 도움말", description="`\\meme` `<정의의후폭풍 | 핵폭탄>`", color=self.embed_color)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        elif argument == "정의의후폭풍":
+            embed = discord.Embed(description="**:rocket: 정의의 후폭풍!**", color=self.embed_color)
+            embed.set_image(url=image_selector(1))
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        elif argument == "핵폭탄":
+            embed = discord.Embed(description="**:boom: 핵 폭발!**", color=self.embed_color)
+            embed.set_image(url=image_selector(1))
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(title=":no_entry: **오류발생!**", description=f"{argument}은(는) 존재하지 않습니다!", color=self.embed_color)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
