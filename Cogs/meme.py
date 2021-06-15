6c8eb856788a80385beafc3420a11a245947ea4a
ProjectTL12345 <projecttl12345@gmail.com>
2021-06-15 22:44:56 +0900

    feat: add some meme

--- Cogs/meme.py
+++ Cogs/meme.py
@@ -15,6 +15,14 @@ def image_selector(number):
         return "http://projecttl.net:8080/s/aJCjtazJYWJpBo8/download/boom.gif"
     elif number == 4:
         return "http://i.imgur.com/DHshyTY.gif"
+    elif number == 5:
+        return "http://projecttl.net:8080/apps/files_sharing/publicpreview/BGyzZLqyF7A7ngT?x=1348&y=315&a=true&file=%25EC%259D%2598%25EC%2582%25AC%25EC%2596%2591%25EB%25B0%2598.jpg&scalingup=0"
+    elif number == 6:
+        return "http://projecttl.net:8080/s/AznzknSXyTkdSwc/download/%EC%9D%98%EC%82%AC%EC%96%91%EB%B0%982.gif"
+    elif number == 7:
+        return "http://projecttl.net:8080/s/NoNyNbxAAL33DqL/download/%EC%84%AC%EA%B4%91%ED%83%84.gif"
+    elif number == 8:
+        return "http://projecttl.net:8080/s/wz3Gzz39c5mmK5T/download/1902___795767943.gif"
 
 class Mime(commands.Cog):
     def __init__(self, bot):
@@ -45,6 +53,26 @@ class Mime(commands.Cog):
         elif argument == "고자라니":
             embed = discord.Embed(description="**:no_entry: 심영짤**", color=self.embed_color)
             embed.set_image(url=image_selector(4))
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        elif argument == "의사양반":
+            embed = discord.Embed(description="**:no_entry: Ah.. 안심하세요, 병원이에요.**", color=self.embed_color)
+            embed.set_image(url=image_selector(5))
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        elif argument == "의사양반2":
+            embed = discord.Embed(description="**:no_entry: Ah.. 안심하세요, 병원이에요.**", color=self.embed_color)
+            embed.set_image(url=image_selector(6))
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        elif argument == "섬광탄":
+            embed = discord.Embed(description="**:no_entry: 눈뽕!**", color=self.embed_color)
+            embed.set_image(url=image_selector(7))
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        elif argument == "파괴":
+            embed = discord.Embed(description="**:no_entry: 널 파괴한다..!**", color=self.embed_color)
+            embed.set_image(url=image_selector(8))
             embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         else:
