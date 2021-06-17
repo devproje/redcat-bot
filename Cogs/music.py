7d976442c07c223e863fa837f8ccccbe21e4dd54
Project_TL <projecttl@Project-TLs-MacBook-Pro.local>
2021-06-17 11:56:27 +0900

    fix: now command

--- Cogs/music.py
+++ Cogs/music.py
@@ -344,7 +344,7 @@ class Music(commands.Cog):
         await ctx.send('Volume of the player set to {}%'.format(volume))
 
     @commands.command(name='now', aliases=['current', 'playing'])
-    async def _now(self, ctx: commands.Context):
+    async def _now(self, ctx):
         """Displays the currently playing song."""
 
         await ctx.send(embed=ctx.voice_state.current.create_embed())
