abece61478f4841b2fa8ba7147dc5374206403e2
ProjectTL12345 <projecttl12345@gmail.com>
2021-07-02 15:08:55 +0900

    TEST

--- Cogs/meme.py
+++ Cogs/meme.py
@@ -33,49 +33,48 @@ class Mime(commands.Cog):
     async def _meme(self, ctx, argument=None):
         if argument is None:
             embed = discord.Embed(title=":question: 밈 도움말", description="`\\meme` `<정의의후폭풍 | 핵폭탄 | 극장폭파 | 의사양반 or 의사양반2 | 섬광탄 | 파괴>`", color=self.embed_color)
-            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator} * Powered by Google Drive", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif argument == "정의의후폭풍":
             embed = discord.Embed(description="**:rocket: 정의의 후폭풍!**", color=self.embed_color)
             embed.set_image(url=image_selector(1))
-            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator} * Powered by Google Drive", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif argument == "핵폭탄":
             embed = discord.Embed(description="**:boom: 핵 폭발!**", color=self.embed_color)
             embed.set_image(url=image_selector(2))
-            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator} * Powered by Google Drive", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif argument == "극장폭파":
             embed = discord.Embed(description="**:boom: 극장 폭파**", color=self.embed_color)
             embed.set_image(url=image_selector(3))
-            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator} * Powered by Google Drive", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif argument == "고자라니":
             embed = discord.Embed(description="**:no_entry: 심영짤**", color=self.embed_color)
             embed.set_image(url=image_selector(4))
-            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator} * Powered by Google Drive", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif argument == "의사양반":
             embed = discord.Embed(description="**:no_entry: Aㅏ 안심하시오, 병원이오.**", color=self.embed_color)
             embed.set_image(url=image_selector(5))
-            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator} * Powered by Google Drive", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif argument == "의사양반2":
             embed = discord.Embed(description="**:no_entry: Aㅏ 안심하시오, 병원이오.**", color=self.embed_color)
             embed.set_image(url=image_selector(6))
-            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator} * Powered by Google Drive", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif argument == "섬광탄":
             embed = discord.Embed(description="**:no_entry: 눈뽕!**", color=self.embed_color)
             embed.set_image(url=image_selector(7))
-            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator} * Powered by Google Drive", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
         elif argument == "파괴":
             embed = discord.Embed(description="**:no_entry: 널 파괴한다..!**", color=self.embed_color)
             embed.set_image(url=image_selector(8))
-            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator} * Powered by Google Drive", icon_url=ctx.author.avatar_url)
         else:
             embed = discord.Embed(title=":no_entry: **오류발생!**", description=f"{argument}은(는) 존재하지 않습니다!", color=self.embed_color)
-            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+            embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator} * Powered by Google Drive", icon_url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
