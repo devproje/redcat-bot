91e653c4f55c9d7ebd1d4f7ced2d9e2d5d27ab91
Project_TL <me@projecttl.net>
2021-07-03 09:57:45 +0900

    feat: slash command profile

--- Cogs/profile_grabber.py
+++ Cogs/profile_grabber.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord_slash import SlashContext, cog_ext
 
 def setup(bot):
     bot.add_cog(ProfileGrabber(bot))
@@ -11,6 +12,15 @@ class ProfileGrabber(commands.Cog):
 
     @commands.command(name="profile")
     async def profile(self, ctx, member: discord.User = None or discord.User.mention == None):
+        member = member or ctx.author
+        embed = discord.Embed(title=f"**{member.display_name}#{member.discriminator}**'s Profile image!", color=self.embed_color)
+        embed.set_image(url=member.avatar_url)
+        embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+
+        await ctx.send(embed=embed)
+
+    @cog_ext.cog_slash(name="profile", description="You can grab target person's avatar image")
+    async def profile(self, ctx: SlashContext, member: discord.User = None or discord.User.mention == None):
         member = member or ctx.author
         embed = discord.Embed(title=f"**{member.display_name}#{member.discriminator}**'s Profile image!", color=self.embed_color)
         embed.set_image(url=member.avatar_url)
