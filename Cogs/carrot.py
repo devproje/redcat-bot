a852a5cbe80edce34c04cad7a87b54d9c5aecded
Project_TL <me@projecttl.net>
2021-07-03 11:28:49 +0900

    fix: carrot

--- Cogs/carrot.py
+++ Cogs/carrot.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord_slash import SlashContext, cog_ext
 
 def setup(bot):
     bot.add_cog(Carrot(bot))
@@ -9,5 +10,10 @@ class Carrot(commands.Cog):
 
     @commands.command(name="carrot")
     async def carrot(self, ctx):
+        await ctx.message.add_reaction("✅")
+        await ctx.send(":carrot:")
+
+    @cog_ext.cog_slash(name="carrot")
+    async def carrot(self, ctx: SlashContext):
         await ctx.message.add_reaction("✅")
         await ctx.send(":carrot:")
