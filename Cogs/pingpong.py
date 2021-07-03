66bcfad07f5ffe5474ef944c37400e177d6d5358
Project_TL <me@projecttl.net>
2021-07-03 10:09:48 +0900

    remove: old code for pingpong

--- Cogs/pingpong.py
+++ Cogs/pingpong.py
@@ -11,20 +11,6 @@ class PingPong(commands.Cog):
         self.bot = bot
         self.embed_color = 0x75B8FF
 
-    @commands.command(name="ping")
-    async def ping(self, ctx):
-        embed = discord.Embed(title=":ping_pong: Pong!", description=f"{round(self.bot.latency * 1000)}ms", color=self.embed_color)
-        embed.set_footer(text=f"{ctx.author.name}{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
-        
-        await ctx.send(embed=embed)
-
-    @commands.command(name="pong")
-    async def pong(self, ctx):
-        embed=discord.Embed(title=":ping_pong: Ping!", description=f"{round(self.bot.latency * 1000)}ms", color=self.embed_color)
-        embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
-        
-        await ctx.send(embed=embed)
-
     @cog_ext.cog_slash(name="ping", description="You can ping pong with bot")
     async def ping(self, ctx):
         embed = discord.Embed(title=":ping_pong: Pong!", description=f"{round(self.bot.latency * 1000)}ms", color=self.embed_color)
