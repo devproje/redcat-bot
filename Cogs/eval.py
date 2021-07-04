76037f6e732f0f2338820ee61d18165c4523e4f3
Project_TL <me@projecttl.net>
2021-07-04 11:15:51 +0900

    test: eval #13

--- Cogs/eval.py
+++ Cogs/eval.py
@@ -2,7 +2,7 @@ from time import time
 from discord.ext import commands
 from inspect import getsource
 from aioconsole import aexec
-import discord, sys, os
+import discord, sys, os, subprocess
 
 def setup(bot):
     bot.add_cog(Eval(bot))
@@ -13,6 +13,18 @@ class Eval(commands.Cog):
     
     @commands.command(name="eval", pass_context=True)
     async def eval(self, ctx, *, code: str):
-        result=exec(code)
-        embed=discord.Embed(title=":white_check_mark: **Python Eval Results**", description=f"```py\n{result}\n```")
+        file = open("code_space.py", mode="wt", encoding="utf-8")
+        file.write(code)
+        file.close()
+
+        source=file.read().strip
+
+        cmd = ["python3.9", "code_space.py"]
+        fd_popen = subprocess.Popen(cmd, stdout=subprocess.PIPE).stdout
+        data = fd_popen.read().strip()
+        data_decode=data.decode("utf-8")
+        fd_popen.close()
+        embed=discord.Embed(title=":white_check_mark: **Python Eval Results**", description=f"**Source**:\n```py\n{source}\n```\n**Result**:\n```sh\n{data_decode}\n```")
         await ctx.send(embed=embed)
+
+        os.system("rm code_space.py")
