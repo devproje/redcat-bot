da75691f35b596f11ee2eadcf180beeb41e2c648
Project_TL <me@projecttl.net>
2021-07-04 10:32:49 +0900

    fix: change eval command type [slash command -> normal command]

--- Cogs/eval.py
+++ Cogs/eval.py
@@ -1,7 +1,5 @@
-from discord import embeds
 import discord
 from discord.ext import commands
-from discord_slash import SlashContext, cog_ext
 from aioconsole import aexec
 
 def setup(bot):
@@ -11,8 +9,8 @@ class Eval(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name="eval", description="Eval code")
-    async def eval(self, ctx: SlashContext, *, type, code: str):
+    @commands.command(name="eval")
+    async def eval(self, ctx, *, type, code: str):
         if type == "py":
             result = await aexec(code)
             embed=discord.Embed(title=":white_check_mark: **Python Eval Results**", description=f"**Code**:\n```py\n{code}\n```\n**Result**:\n```sh\n{result}\n```")
