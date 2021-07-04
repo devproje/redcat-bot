0673b8bfc7c4913cf81b56bc542134ad59bf52b6
Project_TL <me@projecttl.net>
2021-07-04 10:19:48 +0900

    feat: eval

---
+++ Cogs/eval.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+from discord_slash import SlashContext, cog_ext
+from aioconsole import aexec
+
+def setup(bot):
+    bot.add_cog(Eval(bot))
+
+class Eval(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @cog_ext.cog_slash(name="eval", description="Eval code")
+    async def eval(self, ctx: SlashContext, *, type, code):
+        if type == "py":
+            await ctx.send(aexec(code))
