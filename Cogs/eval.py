432abdda8b7bf13ce7c2ad9ba9fbf3c898d04986
Project_TL <me@projecttl.net>
2021-07-04 11:32:20 +0900

    fix

--- Cogs/eval.py
+++ Cogs/eval.py
@@ -13,23 +13,20 @@ class Eval(commands.Cog):
     
     @commands.command(name="eval", pass_context=True)
     async def eval(self, ctx, *, code: str):
-        if ctx.author.id != self.author_id:
-            return None
-        else:
-            code_file = open("code_space.py", mode="w", encoding="utf-8")
-            code_file.write(code)
-            code_file.close()
+        code_file = open("code_space.py", mode="w", encoding="utf-8")
+        code_file.write(code)
+        code_file.close()
 
-            file = open("code_space.py", mode="r", encoding="utf-8")
+        file = open("code_space.py", mode="r", encoding="utf-8")
 
-            source=file.read()
-            file.close()
+        source=file.read()
+        file.close()
         
-            cmd = ["python3.9", "code_space.py"]
-            fd_popen = subprocess.Popen(cmd, stdout=subprocess.PIPE).stdout
-            data = fd_popen.read().strip()
-            data_decode=data.decode("utf-8")
-            fd_popen.close()
+        cmd = ["python3.9", "code_space.py"]
+        fd_popen = subprocess.Popen(cmd, stdout=subprocess.PIPE).stdout
+        data = fd_popen.read().strip()
+        data_decode=data.decode("utf-8")
+        fd_popen.close()
         
-            embed=discord.Embed(title=":white_check_mark: **Python Eval Results**", description=f"**Source**:\n```py\n{source}\n```\n**Result**:\n```sh\n{data_decode}\n```")
-            await ctx.send(embed=embed)
+        embed=discord.Embed(title=":white_check_mark: **Python Eval Results**", description=f"**Source**:\n```py\n{source}\n```\n**Result**:\n```sh\n{data_decode}\n```")
+        await ctx.send(embed=embed)
