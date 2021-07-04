20a4955435561f045690512964d130520ebb53c6
Project_TL <me@projecttl.net>
2021-07-04 09:32:38 +0900

    style: clean code

--- bot.py
+++ bot.py
@@ -46,18 +46,18 @@ def get_uptime():
 @slash.slash(name="status")
 async def status(ctx: SlashContext):
     host = "Hosting by **ADP_Community**"
-    embed=(discord.Embed(title=f"ProjectBot-remake {bot_version} Status", description=f"{host}", color=embed_color))
-    embed.add_field(name="**CPU USAGE**", value=f"__{psutil.cpu_percent()}%__", inline=True)
-    embed.add_field(name="**RAM USAGE**", value=f"__{psutil.virtual_memory().percent}%__", inline=True)
-    embed.add_field(name="**AVAILABLE USAGE**", value=f"__{round(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total, 1)}%__", inline=True)
+    embed=(discord.Embed(title=f"ProjectBot-remake {bot_version} Status", description=f"{host}", color=embed_color)
+        .add_field(name="**CPU USAGE**", value=f"__{psutil.cpu_percent()}%__", inline=True)
+        .add_field(name="**RAM USAGE**", value=f"__{psutil.virtual_memory().percent}%__", inline=True)
+        .add_field(name="**AVAILABLE USAGE**", value=f"__{round(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total, 1)}%__", inline=True)
 
-    embed.add_field(name="**UPTIME**", value=f"__{get_uptime()}__", inline=True)
-    embed.add_field(name="**SYSTEM INFO**", value=f"__{platform.system()} | {platform.machine()}__", inline=True)
-    embed.add_field(name="**PING**", value=f"__{round(bot.latency * 1000)}ms__", inline=True)
+        .add_field(name="**UPTIME**", value=f"__{get_uptime()}__", inline=True)
+        .add_field(name="**SYSTEM INFO**", value=f"__{platform.system()} | {platform.machine()}__", inline=True)
+        .add_field(name="**PING**", value=f"__{round(bot.latency * 1000)}ms__", inline=True)
 
-    embed.add_field(name="OWNER", value=author_name, inline=True)
-    embed.add_field(name="CONTRIBUTERS", value=contributers, inline=True)
-    embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+        .add_field(name="OWNER", value=author_name, inline=True)
+        .add_field(name="CONTRIBUTERS", value=contributers, inline=True)
+        .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
 
     await ctx.send(embed=embed)
 
