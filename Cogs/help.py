b23ee7f8f0caed1a990542ee604fa23a185c37ce
Project_TL <me@projecttl.net>
2021-07-04 09:45:51 +0900

    feat: push extention use slash command

--- Cogs/help.py
+++ Cogs/help.py
@@ -39,11 +39,11 @@ class Help(commands.Cog):
                 .add_field(name="`/version`", value="You can see this bot version", inline=True)
                 .add_field(name="`/clear <amount>`", value="You can remove chat **(Admin or bot owner only)**", inline=True)
                 .add_field(name="`/status`", value="You can see instance status!", inline=True)
+                .add_field(name="`/meme`", value="You can use Project_TL's meme image!", inline=True)
             
                 .add_field(name="**Normal Command**", value="This is normal command help (prefix \"\\\")", inline=False)
                 .add_field(name="`\\help`", value="Open help embed", inline=True)
-                .add_field(name="`\\music help`", value="You can see music command options!", inline=True)
-                .add_field(name="`\\meme`", value="You can use Project_TL's meme image!", inline=True))
+                .add_field(name="`\\music help`", value="You can see music command options!", inline=True))
 
             if ctx.author.id != self.author_id:
                 embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
@@ -54,7 +54,7 @@ class Help(commands.Cog):
                 embed.add_field(name="`/notice <text>`", value="notice command", inline=True)
                 embed.add_field(name="`/load <extension_name>`", value="You can load command", inline=True)
                 embed.add_field(name="`/unload <extension_name>`", value="You can unload command", inline=True)
-                embed.add_field(name="`\\push`", value="You can push instance code", inline=True)
+                embed.add_field(name="`/push`", value="You can push instance code", inline=True)
                 embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
 
             await ctx.channel.send(embed=embed)
--- Cogs/push.py
+++ Cogs/push.py
@@ -1,5 +1,6 @@
 import discord, os, subprocess
 from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
 
 def setup(bot):
     bot.add_cog(Push(bot))
@@ -9,8 +10,8 @@ class Push(commands.Cog):
         self.bot = bot
         self.author_id = 415801068174180352
 
-    @commands.command(name="push")
-    async def push_code(self, ctx):
+    @cog_ext.cog_slash(name="push")
+    async def push_code(self, ctx: SlashContext):
         if ctx.author.id != self.author_id:
             embed = discord.Embed(title=f":stop_sign: **Error!**", description=f"You can push code!\nBecause you're not bot owner!")
             await ctx.send(embed=embed)
