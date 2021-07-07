6b53a836e3ade04da6a486bc3d0fd12f232fc846
ProjectTL12345 <projecttl12345@gmail.com>
2021-07-08 06:35:02 +0900

    test: Transaction Error

--- Cogs/update.py
+++ Cogs/update.py
@@ -13,6 +13,8 @@ class Update(commands.Cog):
     
     @cog_ext.cog_slash(name="update", description="Bot owner can update source with github repositories")
     async def update_code(self, ctx: SlashContext):
+        await ctx.respond()
+
         if ctx.author.id != self.author_id:
             embed = discord.Embed(name=":stop_sign: Update fail!", description="You're not bot owner!", color=self.embed_color)
             embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
