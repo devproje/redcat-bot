4e46b1b865a303a807509c0f42b1fedc0d23aa18
Project_TL <me@projecttl.net>
2021-07-07 15:35:21 +0900

    fix: filename change profile_grabber -> avatar

--- Cogs/avatar.py
+++ Cogs/avatar.py
@@ -3,17 +3,17 @@ from discord.ext import commands
 from discord_slash import SlashContext, cog_ext
 
 def setup(bot):
-    bot.add_cog(ProfileGrabber(bot))
+    bot.add_cog(Avatar(bot))
 
-class ProfileGrabber(commands.Cog):
+class Avatar(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.embed_color = 0x75B8FF
 
-    @cog_ext.cog_slash(name="profile", description="You can grab target person's avatar image")
-    async def profile(self, ctx: SlashContext, member: discord.User = None or discord.User.mention == None):
+    @cog_ext.cog_slash(name="avatar", description="You can grab target person's avatar image")
+    async def avatar(self, ctx: SlashContext, member: discord.User = None or discord.User.mention == None):
         member = member or ctx.author
-        embed = discord.Embed(title=f"**{member.display_name}#{member.discriminator}**'s Profile image!", color=self.embed_color)
+        embed = discord.Embed(title=f"**{member.display_name}#{member.discriminator}**'s Avatar!", color=self.embed_color)
         embed.set_image(url=member.avatar_url)
         embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
 
