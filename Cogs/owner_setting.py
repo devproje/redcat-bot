83b68112e6df57c74d01bec66029d3594a057af7
ProjectTL12345 <projecttl12345@gmail.com>
2021-07-08 11:15:17 +0900

    feat: extention

--- Cogs/owner_setting.py
+++ Cogs/owner_setting.py
@@ -13,31 +13,28 @@ class OwnerSetting(commands.Cog):
         self.owner_id = 415801068174180352
 
     @cog_ext.cog_slash(name="set_avatar", description="Owner's Settings!")
-    async def settings(self, ctx, *, type):
+    async def settings(self, ctx, *, extention, type):
         if ctx.author.id == self.owner_id:
-            if type == "light":
-                embed = (discord.Embed(title=":white_check_mark: **Done!**", description="CatBot's avatar mode is **light** mode!", color=self.embed_color)
+            if extention == None:
+                embed = (discord.Embed(title=":no_entry: **Error**", description="Extention must not None", color=self.embed_color)
                     .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
-
-                with open('profile_image/light.png', 'rb') as profile_image:
-                    await self.bot.user.edit(avatar=profile_image.read())
-                
                 await ctx.send(embed=embed)
-                
-            elif type == "dark":
-                embed = (discord.Embed(title=":white_check_mark: **Done!**", description="CatBot's avatar mode is **dark** mode!", color=self.embed_color)
-                    .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
-                
-                with open('profile_image/dark.png', 'rb') as profile_image:
-                    await self.bot.user.edit(avatar=profile_image.read())
 
-                await ctx.send(embed=embed)
-            
             else:
-                embed = (discord.Embed(title=":no_entry: **Error!**", description=f"Invalid type `{type}`", color=self.embed_color)
-                    .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+                if type == None:
+                    embed = (discord.Embed(title=":no_entry: **Error**", description="Type must not None", color=self.embed_color)
+                        .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+                    await ctx.send(embed=embed)
 
-                await ctx.send(embed=embed)
+                else:
+                    embed = (discord.Embed(title=":white_check_mark: **Done!**", description="CatBot's avatar mode is **light** mode!", color=self.embed_color)
+                        .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+
+                    with open(f'profile_image/{extention}_{type}.png', 'rb') as profile_image:
+                        await self.bot.user.edit(avatar=profile_image.read())
+                
+                    await ctx.send(embed=embed)
+
         else:
             embed = (discord.Embed(title=":no_entry: **Error!**", description="You're not bot owner!", color=self.embed_color)
                 .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
