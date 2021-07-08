b6928b7eb7a574ea672b24b258a6f629ae6760f8
ProjectTL12345 <projecttl12345@gmail.com>
2021-07-08 15:57:54 +0900

    feat: maintance function

--- Cogs/avatar.py
+++ Cogs/avatar.py
@@ -5,16 +5,28 @@ from discord_slash import SlashContext, cog_ext
 def setup(bot):
     bot.add_cog(Avatar(bot))
 
+maintance_file="maintance.txt"
+open_maintance=open(maintance_file, "r", encoding="utf-8")
+maintance=open_maintance.read().split[0]
+
 class Avatar(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.embed_color = 0xED4245
 
-    @cog_ext.cog_slash(name="avatar", description="You can grab target person's avatar image")
-    async def avatar(self, ctx: SlashContext, member: discord.User = None or discord.User.mention == None):
+        self.owner_id = 415801068174180352
+
+    async def avatar_grabber(ctx, member):
         member = member or ctx.author
         embed = discord.Embed(title=f":frame_photo: **{member.display_name}#{member.discriminator}**'s Avatar!", color=self.embed_color)
         embed.set_image(url=member.avatar_url)
         embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
 
         await ctx.send(embed=embed)
+
+    @cog_ext.cog_slash(name="avatar", description="You can grab target person's avatar image")
+    async def avatar(self, ctx: SlashContext, member: discord.User = None or discord.User.mention == None):
+        if maintance == "true":
+            self.avatar_grabber(ctx, member)
+        else:
+            self.avatar_grabber(ctx, member)
--- bot.py
+++ bot.py
@@ -17,6 +17,10 @@ owner_id = 415801068174180352
 
 host_name="ADP_Community"
 
+maintance_file="maintance.txt"
+open_maintance=open(maintance_file, "r", encoding="utf-8")
+maintance=open_maintance.read().split[0]
+
 token_path = "token.txt"
 open_token = open(token_path, "r", encoding = "utf-8")
 token = open_token.read().split()[0]
@@ -31,23 +35,34 @@ now = datetime.now()
 @bot.event
 async def on_ready():
     print(f"Logined for {bot_name}")
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(f"{bot_name} {bot_version}"))
-    
     activity = [f"{bot_name} {bot_version}", f"Uptime: {get_uptime()}"]
-    await activity_switcher(activity)
 
-async def activity_switcher(games):
+    if maintance == "true":
+        with open(f'profile_image/maintance/dark.png', 'rb') as profile_image:
+            await bot.user.edit(avatar=profile_image.read())
+        activity_switcher(activity)
+    else:
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game(f"{bot_name} {bot_version}"))
+        await activity_switcher(activity)
+
+async def activity_switcher(activity):
     await bot.wait_until_ready()
-
-    while not bot.is_closed():
-        for g in games:
-            await bot.change_presence(activity = discord.Game(g))
-            await asyncio.sleep(20)
+    
+    if not maintance == "true":
+        while not bot.is_closed():
+            for g in activity:
+                await bot.change_presence(activity = discord.Game(g))
+                await asyncio.sleep(20)
+    else:
+        maintance_activity = [f"{bot_name} is maintance", "⛔️ MAINTANCE"]
+        await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game(f"{bot_name} is maintance"))
+        while not bot.is_closed():
+            for act in maintance_activity:
+                await bot.change_presence(activity = discord.Game(act))
 
 host = f"Hosting by **{host_name}**"
 
-@slash.slash(name="botinfo", description="You can show bot info.")
-async def version(ctx: SlashContext):
+async def run_bot_info(ctx):
     await ctx.defer()
 
     embed = (discord.Embed(title=f":dart: **{bot_name}** info", description=f"{host}", color=embed_color)
@@ -62,14 +77,24 @@ async def version(ctx: SlashContext):
         .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
     await ctx.channel.send(embed=embed)
 
+@slash.slash(name="botinfo", description="You can show bot info.")
+async def version(ctx: SlashContext):
+    if maintance == "true":
+        if ctx.author_id == owner_id:
+            run_bot_info(ctx)
+        else:
+            return None
+
+    else:
+        run_bot_info(ctx)
+
 def get_uptime():
     with open("/proc/uptime", 'r') as f:
         uptime_seconds = float(f.readline().split()[0])
         t = timedelta(seconds = uptime_seconds)
         return ("{} D | {} H | {} M".format(t.days, t.seconds // 3600, (t.seconds // 60) % 60))
 
-@slash.slash(name="status")
-async def status(ctx: SlashContext):
+async def run_status(ctx):
     embed=(discord.Embed(title=f"{bot_name} {bot_version} Status", description=f"{host}", color=embed_color)
         .add_field(name="**CPU USAGE**", value=f"__{psutil.cpu_percent()}%__", inline=True)
         .add_field(name="**RAM USAGE**", value=f"__{psutil.virtual_memory().percent}%__", inline=True)
@@ -87,8 +112,24 @@ async def status(ctx: SlashContext):
 
     await ctx.send(embed=embed)
 
+@slash.slash(name="status")
+async def status(ctx: SlashContext):
+    if maintance == "true":
+        if ctx.author_id == owner_id:
+            run_status(ctx)
+        else:
+            return None
+    else:
+        run_status(ctx)
+
 @slash.slash(name="version")
 async def version(ctx: SlashContext):
-    await ctx.send(f"My version is {bot_version}")
+    if maintance == "true":
+        if ctx.author_id == owner_id:
+            await ctx.send(f"My version is {bot_version}")
+        else:
+            return None
+    else:
+        await ctx.send(f"My version is {bot_version}")
 
 bot.run(token)
