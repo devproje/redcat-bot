ad41517539504042c12aeb4e17246ae3c467e5d6
Project_TL <me@projecttl.net>
2021-07-08 13:40:13 +0900

    featt: add help function

--- Cogs/help.py
+++ Cogs/help.py
@@ -14,29 +14,9 @@ class Help(commands.Cog):
     @cog_ext.cog_slash(name="help", description="You can see commands list!")
     async def help(self, ctx: SlashContext, argument=None):
         await ctx.defer()
+        helper=EmbedHelper()
 
-        if argument == "music":
-            await ctx.send(embed=help_music(ctx, self.embed_color))
-
-        elif argument == "ping":
-            await ctx.send(embed=ping_pong(ctx, self.embed_color, False))
-
-        elif argument == "pong":
-            await ctx.send(embed=ping_pong(ctx, self.embed_color, True))
-
-        elif argument == "carrot":
-            await ctx.send(embed=carrot(ctx, self.embed_color))
-        
-        elif argument == "wincho":
-            await ctx.send(embed=wincho(ctx, self.embed_color, False))
-
-        elif argument == "윈초":
-            await ctx.send(embed=wincho(ctx, self.embed_color, True))
-        
-        elif argument == "avatar":
-            await ctx.send(embed=avatar(ctx, self.embed_color))
-
-        elif argument == None:
+        if argument == None:
             embed = (discord.Embed(title=":dart: **Help**", description="**This is command list**", color=self.embed_color)
                 .add_field(name="**Slash Command**", value="Slash command help\n**This feature is beta!**", inline=False)
                 .add_field(name="`/ping`", value="You can ping pong with bot", inline=True)
@@ -66,56 +46,62 @@ class Help(commands.Cog):
                 embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
 
             await ctx.channel.send(embed=embed)
+        
+        elif argument == "music":
+            await ctx.send(embed=helper.help_music(ctx, self.embed_color))
 
+        elif argument == "ping":
+            await ctx.send(embed=helper.ping_pong(ctx, self.embed_color, False))
+
+        elif argument == "pong":
+            await ctx.send(embed=helper.ping_pong(ctx, self.embed_color, True))
+
+        elif argument == "carrot":
+            await ctx.send(embed=helper.carrot(ctx, self.embed_color))
+        
+        elif argument == "wincho":
+            await ctx.send(embed=helper.wincho(ctx, self.embed_color, False))
+
+        elif argument == "윈초":
+            await ctx.send(embed=helper.wincho(ctx, self.embed_color, True))
+        
+        elif argument == "avatar":
+            await ctx.send(embed=helper.avatar(ctx, self.embed_color))
+        
         else:
             embed = (discord.Embed(title=":no_entry_sign: **Error!**", description=f"**{argument}** is not exist", color=self.embed_color))
             await ctx.channel.send(embed=embed)
 
-def avatar(ctx, embed_color):
-    embed = (discord.Embed(title=":frame_photo: **Avatar Help**", description="If you want use this, please mention person!\n(Notification is not sented with mention person)", color=self.embed_color)
-        .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
-    return embed
-
-def wincho(ctx, embed_color, korean):
-    if not korean == True:
-        embed = (discord.Embed(title=":question: **Wincho Help**", description="`/wincho` `<burn | smash | melt | call | 프젝기술>`", color=0xFFF1D6)
+class EmbedHelper():
+    def help_music(self, ctx, embed_color):
+        embed = (discord.Embed(title=":musical_note: **Music Help**", description="This is music options!", color=embed_color)
+            .add_field(name="`\\play <url or name>`", value="Play music", inline=True)
+            .add_field(name="`\\pause`", value="Pause music", inline=True)
+            .add_field(name="`\\resume`", value="Resume music", inline=True)
+            .add_field(name="`\\now`", value="Checking playing music", inline=True)
+            .add_field(name="`\\queue`", value="Checking play list", inline=True)
+            .add_field(name="`\\stop`", value="Stop all music", inline=True)
+            .add_field(name="`\\skip`", value="Skip current music", inline=True)
+            .add_field(name="`\\shuffle`", value="Shuffle current queue", inline=True)
+            .add_field(name="`\\loop`", value="Loop current music", inline=True)
+            .add_field(name="`\\remove <array_number>`", value="Remove queued target music", inline=True)
+            .add_field(name="`\\leave`", value="Leave bot", inline=True)
             .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+
         return embed
     
-    else:
-        embed = (discord.Embed(title=":question: **윈초 커맨드 도움말**", description="`/윈초` `<녹이기 | 부수기 | 녹이기 | 부르기 | 프젝기술>`", color=0xFFF1D6)
+    def ping_pong(self, ctx, embed_color, pong):
+        if pong == False:
+            embed = (discord.Embed(title=":ping_pong: **Ping Help**", description="If you type `/ping`, bot will respond with pong!", color=embed_color)
+                .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+            return embed
+
+        else:
+            embed = (discord.Embed(title=":ping_pong: **Pong Help**", description="If you type `/pong`, bot will respond with ping!", color=embed_color)
+                .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
+            return embed
+
+    def carrot(self, ctx, embed_color):
+        embed = (discord.Embed(title=":carrot: **Carrot Help**", description="If you type `/carrot`, Bot will throw carrot.", color=embed_color)
             .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
         return embed
-
-def carrot(ctx, embed_color):
-    embed = (discord.Embed(title=":carrot: **Carrot Help**", description="If you type `/carrot`, Bot will throw carrot.", color=embed_color)
-        .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
-    return embed
-
-def ping_pong(ctx, embed_color, pong):
-    if pong == False:
-        embed = (discord.Embed(title=":ping_pong: **Ping Help**", description="If you type `/ping`, bot will respond with pong!", color=embed_color)
-            .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
-        return embed
-
-    else:
-        embed = (discord.Embed(title=":ping_pong: **Pong Help**", description="If you type `/pong`, bot will respond with ping!", color=embed_color)
-            .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
-        return embed     
-        
-def help_music(ctx, embed_color):
-    embed = (discord.Embed(title=":musical_note: **Music Help**", description="This is music options!", color=embed_color)
-        .add_field(name="`\\play <url or name>`", value="Play music", inline=True)
-        .add_field(name="`\\pause`", value="Pause music", inline=True)
-        .add_field(name="`\\resume`", value="Resume music", inline=True)
-        .add_field(name="`\\now`", value="Checking playing music", inline=True)
-        .add_field(name="`\\queue`", value="Checking play list", inline=True)
-        .add_field(name="`\\stop`", value="Stop all music", inline=True)
-        .add_field(name="`\\skip`", value="Skip current music", inline=True)
-        .add_field(name="`\\shuffle`", value="Shuffle current queue", inline=True)
-        .add_field(name="`\\loop`", value="Loop current music", inline=True)
-        .add_field(name="`\\remove <array_number>`", value="Remove queued target music", inline=True)
-        .add_field(name="`\\leave`", value="Leave bot", inline=True)
-        .set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url))
-
-    return embed
